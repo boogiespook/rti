631fdc76205142e8f6844556def81316d7371fcc
Tim Jackson <tim@timj.co.uk>
2017-05-11 21:51:10 +0200

    Refactor to store assessment data early, and redirect to the results page passing only the assessment unique reference ("hash"), thus allowing a URL to the results page to be more easily shared. Also generates shorter airline-style references.

--- tmp.php
+++ tmp.php
@@ -1,7 +1,20 @@
 <?php
-#phpinfo();
-parse_str($_SERVER["QUERY_STRING"]);
-$redirect="results.php?name=" . str_replace(array('&','<','>','/','\\','"',"'",'?','+'), '',htmlentities($customerName)) . "&country=$country&rhEmail=$rhEmail&d1=$d1&o1=$o1&d2=$d2&o2=$o2&d3=$d3&o3=$o3&d4=$d4&o4=$o4&d5=$d5&o5=$o5&id=$userId&status=Completed";
-print "<meta http-equiv='refresh' content='0; url=" . $redirect . "' />";
+include_once 'dbconnect.php';
+connectDB();
 
-?>
+parse_str($_SERVER["QUERY_STRING"], $data);
+$data['client'] = $data['customerName']; // FIXME hack
+
+$chars = '23456789ABCDEFGHJKLMNPQRSTUVWXYZ';
+$data['hash'] = substr(str_shuffle($chars), 0, 5);
+
+$fields = array('client','rhEmail','country','o1','o2','o3','o4','o5','d1','d2','d3','d4','d5','hash');
+foreach ($fields as $field) {
+	$$field = mysqli_real_escape_string($db, $data[$field]);
+}
+$qq = "INSERT INTO data (" . implode(',', $fields).") VALUES ('$client','$rhEmail','$country',$o1,$o2,$o3,$o4,$o5,$d1,$d2,$d3,$d4,$d5,'$hash')";
+$result = mysqli_query($db, $qq);
+
+// TODO check $result
+
+header("Location: results.php?hash=$hash");
