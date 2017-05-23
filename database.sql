2ca81fd643be15b7cf94338b20be2f79e4ed7274
ChrisJ <cj@chrisj.com>
2017-05-23 10:50:46 +0100

    User now saved in dbase when assessment submitted

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 10.169.0.138
--- Generation Time: May 16, 2017 at 07:55 AM
+-- Generation Time: May 23, 2017 at 10:49 AM
 -- Server version: 5.7.16
 -- PHP Version: 5.3.3
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `readytoi_spider`
+-- Database: `spider`
 --
 
 -- --------------------------------------------------------
@@ -28,6 +28,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `data` (
   `id` int(3) NOT NULL,
+  `user` varchar(100) NOT NULL,
   `client` varchar(50) NOT NULL,
   `rhEmail` varchar(50) NOT NULL,
   `country` varchar(100) NOT NULL,
@@ -47,6 +48,48 @@ CREATE TABLE IF NOT EXISTS `data` (
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test_data`
+--
+
+CREATE TABLE IF NOT EXISTS `test_data` (
+  `id` int(3) NOT NULL,
+  `client` varchar(50) NOT NULL,
+  `rhEmail` varchar(50) NOT NULL,
+  `country` varchar(100) NOT NULL,
+  `region` varchar(20) NOT NULL,
+  `lob` int(100) NOT NULL,
+  `o1` int(1) NOT NULL,
+  `o2` int(1) NOT NULL,
+  `o3` int(1) NOT NULL,
+  `o4` int(1) NOT NULL,
+  `o5` int(1) NOT NULL,
+  `d1` int(1) NOT NULL,
+  `d2` int(1) NOT NULL,
+  `d3` int(1) NOT NULL,
+  `d4` int(1) NOT NULL,
+  `d5` int(1) NOT NULL,
+  `hash` varchar(50) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(3) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `name` text NOT NULL,
+  `uuid` varchar(50) NOT NULL,
+  `lastUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Indexes for dumped tables
 --
@@ -59,6 +102,19 @@ ALTER TABLE `data`
   ADD UNIQUE KEY `hash` (`hash`);
 
 --
+-- Indexes for table `test_data`
+--
+ALTER TABLE `test_data`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `hash` (`hash`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -66,6 +122,16 @@ ALTER TABLE `data`
 -- AUTO_INCREMENT for table `data`
 --
 ALTER TABLE `data`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `test_data`
+--
+ALTER TABLE `test_data`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
   MODIFY `id` int(3) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- tmp.php
+++ tmp.php
@@ -1,18 +1,24 @@
 <?php
 include_once 'dbconnect.php';
 connectDB();
-
+#phpinfo();
 parse_str($_SERVER["QUERY_STRING"], $data);
 $data['client'] = $data['customerName']; // FIXME hack
 
 $chars = '23456789ABCDEFGHJKLMNPQRSTUVWXYZ';
 $data['hash'] = substr(str_shuffle($chars), 0, 5);
 
-$fields = array('client','rhEmail','country','lob','o1','o2','o3','o4','o5','d1','d2','d3','d4','d5','hash');
+## Get the username from the userId
+$q1 = "select name from users where id = '" . $data['userId'] . "'";
+$res = mysqli_query($db, $q1);
+$row = mysqli_fetch_assoc($res);
+$data['user'] = $row['name'];
+
+$fields = array('user','client','rhEmail','country','lob','o1','o2','o3','o4','o5','d1','d2','d3','d4','d5','hash');
 foreach ($fields as $field) {
 	$$field = mysqli_real_escape_string($db, $data[$field]);
 }
-$qq = "INSERT INTO data (" . implode(',', $fields).") VALUES ('$client','$rhEmail','$country','$lob',$o1,$o2,$o3,$o4,$o5,$d1,$d2,$d3,$d4,$d5,'$hash')";
+$qq = "INSERT INTO data (" . implode(',', $fields).") VALUES ('$user','$client','$rhEmail','$country','$lob',$o1,$o2,$o3,$o4,$o5,$d1,$d2,$d3,$d4,$d5,'$hash')";
 $result = mysqli_query($db, $qq);
 
 // TODO check $result
