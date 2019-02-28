64b7fefb10b9f1c7e2607e4c7ee4a8c852ba46a8
Chris Jenkins <cjenkins@redhat.com>
2019-02-28 12:58:26 +0000

    CSS removed from HTML and version added

--- register.php
+++ register.php
@@ -64,7 +64,7 @@ curl_setopt_array($curl, array(
     CURLOPT_URL => 'https://www.google.com/recaptcha/api/siteverify',
     CURLOPT_POST => 1,
     CURLOPT_POSTFIELDS => array(
-        'secret' => '6LdWYl0UAAAAAF-OOWcmOaFeiBFamk36-G6BVAHq',
+        'secret' => 'XXXXXXXXXXXXXXXXXXX',
         'response' => $_POST['g-recaptcha-response']
     )
 ));
@@ -159,7 +159,7 @@ if(strpos($resp, '"success": true') !== FALSE) {
         <?php #echo Securimage::getCaptchaHtml() ?>
         						<span class="text-danger"><?php if (isset($captcha_error)) echo "<br>$captcha_error"; ?></span>
     </div>
-<div class="g-recaptcha" data-sitekey="6LdWYl0UAAAAAP8dt8bpKnfqtWKpAZ8bnNHK09-b"></div>
+<div class="g-recaptcha" data-sitekey="XXXXXXXXXXXXXXXXXXXXXXXXXX"></div>
 					<div class="form-group">
 						<input type="submit" name="signup" value="Sign Up" class="btn btn-primary" />
 					</div>
